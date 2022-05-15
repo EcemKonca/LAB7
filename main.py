b37fb7cb5c308f319145385c9c783e49a5349a78
User <ecemkonca@hotmail.com>
2022-05-15 17:13:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("/Users/User/Desktop/colours.jpeg")
+cv2.imshow("Picture", img)
+(b, g, r) = cv2.split(img)
+cv2.imshow("Blue", b)
+cv2.imshow("Green", g)
+cv2.imshow("Red", r)
+
+print(img.shape)
+
+minimum = np.array([10, 20, 30])
+maximum = np.array([240, 245, 250])
+r = cv2.inRange(img, minimum, maximum)
+
+merge = cv2.merge([b, g, r])
+cv2.imshow("Merged Picture", merge)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
